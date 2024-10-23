c14929eaffb8d3be80121cb2744385a4e212857d
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-10-23 21:01:47 +0200

    reslover trendyFilms

---
+++ api/src/resolvers/Film.resolvers.ts
@@ -0,0 +1,28 @@
+import { Film } from "../entities/Film";
+import { Arg, Query, Resolver } from "type-graphql";
+
+@Resolver(Film)
+export class FilmResolver {
+  @Query(() => [Film])
+  async films(): Promise<Film[]> {
+    return Film.find();
+  }
+
+  @Query(() => Film, { nullable: true })
+  async film(@Arg("id") id: number): Promise<Film | null> {
+    return Film.findOneBy({ id });
+  }
+
+  @Query(() => [Film], { nullable: true })
+  async trendyFilms(
+    @Arg("limit", { nullable: true, defaultValue: 3 }) limit: number
+  ): Promise<Film[]> {
+    return Film.find({
+      order: {
+        releaseDate: "DESC",
+        popularity: "DESC",
+      },
+      take: limit,
+    });
+  }
+}
