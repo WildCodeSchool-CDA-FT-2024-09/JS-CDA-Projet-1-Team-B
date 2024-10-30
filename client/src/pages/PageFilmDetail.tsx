ccbfac9ccd6a6eef38dfea52a4ca7263f5bc007e
brewost <156411525+brewost@users.noreply.github.com>
2024-10-30 19:01:56 +0100

    changed the divs to other elements added padding and space with margins

--- client/src/pages/PageFilmDetail.tsx
+++ client/src/pages/PageFilmDetail.tsx
@@ -4,7 +4,6 @@ const FilmDetail = () => {
   // Hardcode the film ID
   const hardcodedFilmId = 1; // Replace this with the desired film ID
   const basePosterUrl = "https://image.tmdb.org/t/p/original/"; // Base URL for TMDB posters
-
   const { loading, error, data } = useGetFilmByIdQuery({
     variables: { getFilmByIdId: hardcodedFilmId },
   });
@@ -20,7 +19,7 @@ const FilmDetail = () => {
   return (
     <main className="flex flex-col items-center p-4 relative">
       {film.posterPath && (
-        <div className="relative w-[30rem] h-[45rem] mt-5 mb-10">
+        <section className="relative w-[30rem] h-[45rem] mt-5 mb-10">
           <div
             className="absolute w-full h-full bg-bloodRed rounded-2xl"
             style={{
@@ -35,32 +34,39 @@ const FilmDetail = () => {
             className="w-full h-full object-cover rounded-2xl shadow-lg border-2 border-white relative"
             style={{ zIndex: 1 }}
           />
-        </div>
+        </section>
       )}
-      <div className="w-[70%] space-y-4 border border-bloodRed rounded p-6 mt-6 text-white text-left">
-        <h1 className="text-2xl font-bold mb-4">{film.title}</h1>
-        <p>
-          <strong className="font-semibold">Overview:</strong> {film.overview}
-        </p>
-        <p>
-          <strong className="font-semibold">Release Date:</strong>{" "}
-          {film.releaseDate}
-        </p>
-        <p>
-          <strong className="font-semibold">Actors:</strong> {film.actors}
-        </p>
-        <p>
-          <strong className="font-semibold">Director:</strong> {film.director}
-        </p>
-        <p>
-          <strong className="font-semibold">Popularity:</strong>{" "}
-          {film.popularity}
-        </p>
-        <p>
-          <strong className="font-semibold">Original Language:</strong>{" "}
-          {film.originalLanguage}
-        </p>
-      </div>
+      <article className="w-[70%] space-y-4 border border-bloodRed rounded p-6 mt-6 text-white text-left">
+        <header>
+          <h1 className="text-2xl font-bold mb-4">{film.title}</h1>
+        </header>
+        <section>
+          <p className="mb-4">
+            <strong className="font-semibold mr-2">Overview:</strong>{" "}
+            {film.overview}
+          </p>
+          <p className="mb-4">
+            <strong className="font-semibold mr-2">Release Date:</strong>
+            {film.releaseDate}
+          </p>
+          <p className="mb-4">
+            <strong className="font-semibold mr-2">Actors:</strong>{" "}
+            {film.actors}
+          </p>
+          <p className="mb-4">
+            <strong className="font-semibold mr-2">Director:</strong>{" "}
+            {film.director}
+          </p>
+          <p className="mb-4">
+            <strong className="font-semibold mr-2">Popularity:</strong>
+            {film.popularity}
+          </p>
+          <p className="mb-4">
+            <strong className="font-semibold mr-2">Original Language:</strong>
+            {film.originalLanguage}
+          </p>
+        </section>
+      </article>
     </main>
   );
 };
