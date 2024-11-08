44acccd7ab06dacf8892ed505b2dea5fe6516753
JEANFRANCOIS HAFID <hafidjeanfrancois@gmail.com>
2024-11-08 12:18:19 +0100

    merge dev

--- client/src/generated/graphql-types.ts
+++ client/src/generated/graphql-types.ts
@@ -107,6 +107,7 @@ export type Query = {
   __typename?: "Query";
   FrenchFilms?: Maybe<Array<Film>>;
   categories: Array<Category>;
+  filmComments?: Maybe<Array<UserComment>>;
   getCategoryById?: Maybe<Category>;
   getFilmById?: Maybe<Film>;
   getUserByEmail?: Maybe<User>;
@@ -121,6 +122,11 @@ export type Query = {
 
 export type QueryFrenchFilmsArgs = {
   limit?: InputMaybe<Scalars["Float"]["input"]>;
+};
+
+
+export type QueryFilmCommentsArgs = {
+  filmId: Scalars["Int"]["input"];
 };
 
 
@@ -186,6 +192,7 @@ export type UserComment = {
   content: Scalars["String"]["output"];
   created_at: Scalars["DateTimeISO"]["output"];
   film: Film;
+  id: Scalars["ID"]["output"];
   updated_at: Scalars["DateTimeISO"]["output"];
   user: User;
 };
@@ -251,6 +258,23 @@ export type GetCategoryByIdQueryVariables = Exact<{
 export type GetCategoryByIdQuery = {
   __typename?: "Query",
   getCategoryById?: { __typename?: "Category", id: number, name: string } | null
+};
+
+export type FilmCommentsQueryVariables = Exact<{
+  filmId: Scalars["Int"]["input"];
+}>;
+
+
+export type FilmCommentsQuery = {
+  __typename?: "Query",
+  filmComments?: Array<{
+    __typename?: "UserComment",
+    id: string,
+    content: string,
+    created_at: any,
+    updated_at: any,
+    user: { __typename?: "User", username: string, avatar: { __typename?: "Avatar", image: string, id: string } }
+  }> | null
 };
 
 export type FrenchFilmsQueryVariables = Exact<{ [key: string]: never; }>;
@@ -582,6 +606,62 @@ export type GetCategoryByIdQueryHookResult = ReturnType<typeof useGetCategoryByI
 export type GetCategoryByIdLazyQueryHookResult = ReturnType<typeof useGetCategoryByIdLazyQuery>;
 export type GetCategoryByIdSuspenseQueryHookResult = ReturnType<typeof useGetCategoryByIdSuspenseQuery>;
 export type GetCategoryByIdQueryResult = Apollo.QueryResult<GetCategoryByIdQuery, GetCategoryByIdQueryVariables>;
+export const FilmCommentsDocument = gql`
+    query FilmComments($filmId: Int!) {
+        filmComments(filmId: $filmId) {
+            id
+            content
+            created_at
+            updated_at
+            user {
+                username
+                avatar {
+                    image
+                    id
+                }
+            }
+        }
+    }
+`;
+
+/**
+ * __useFilmCommentsQuery__
+ *
+ * To run a query within a React component, call `useFilmCommentsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useFilmCommentsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useFilmCommentsQuery({
+ *   variables: {
+ *      filmId: // value for 'filmId'
+ *   },
+ * });
+ */
+export function useFilmCommentsQuery(baseOptions: Apollo.QueryHookOptions<FilmCommentsQuery, FilmCommentsQueryVariables> & ({
+  variables: FilmCommentsQueryVariables;
+  skip?: boolean;
+} | { skip: boolean; })) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useQuery<FilmCommentsQuery, FilmCommentsQueryVariables>(FilmCommentsDocument, options);
+}
+
+export function useFilmCommentsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<FilmCommentsQuery, FilmCommentsQueryVariables>) {
+  const options = { ...defaultOptions, ...baseOptions };
+  return Apollo.useLazyQuery<FilmCommentsQuery, FilmCommentsQueryVariables>(FilmCommentsDocument, options);
+}
+
+export function useFilmCommentsSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<FilmCommentsQuery, FilmCommentsQueryVariables>) {
+  const options = baseOptions === Apollo.skipToken ? baseOptions : { ...defaultOptions, ...baseOptions };
+  return Apollo.useSuspenseQuery<FilmCommentsQuery, FilmCommentsQueryVariables>(FilmCommentsDocument, options);
+}
+
+export type FilmCommentsQueryHookResult = ReturnType<typeof useFilmCommentsQuery>;
+export type FilmCommentsLazyQueryHookResult = ReturnType<typeof useFilmCommentsLazyQuery>;
+export type FilmCommentsSuspenseQueryHookResult = ReturnType<typeof useFilmCommentsSuspenseQuery>;
+export type FilmCommentsQueryResult = Apollo.QueryResult<FilmCommentsQuery, FilmCommentsQueryVariables>;
 export const FrenchFilmsDocument = gql`
     query FrenchFilms {
         FrenchFilms {
